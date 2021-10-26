de85740ddae91ad9f00a77bdc8faaaad2696dd0c
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2021-10-26 09:09:32 +0200

    Added source code that was missing

---
+++ app/src/main/kotlin/com/marcohc/terminator/sample/MainApplication.kt
@@ -0,0 +1,25 @@
+package com.marcohc.terminator.sample
+
+import android.app.Application
+import com.marcohc.terminator.sample.di.ApplicationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+@Suppress("unused")
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@MainApplication)
+            modules(ApplicationModule.modules)
+        }
+    }
+}
